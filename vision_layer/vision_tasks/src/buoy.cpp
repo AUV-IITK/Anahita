eb98a7a0168afc871bc9de576ce5cc3a88f602c2
Naishadh Parmar <naishadh_parmar@live.com>
2018-06-11 23:47:53 +0530

    Fix the -215 error in `vision_commons`.

--- vision_layer/vision_tasks/src/buoy.cpp
+++ vision_layer/vision_tasks/src/buoy.cpp
@@ -18,7 +18,7 @@
 #include <vision_commons/morph.h>
 #include <vision_commons/threshold.h>
 
-int low_h, low_s, low_v, high_h, high_s, high_v;
+int low_h, low_s, low_v, high_h, high_s, high_v, opening_closing_mat_point, opening_iter, closing_iter;
 image_transport::Publisher thresholded_HSV_pub;
 image_transport::Publisher marked_pub;
 ros::Publisher coordinates_pub;
@@ -32,58 +32,52 @@ void callback(vision_tasks::rangeConfig &config, double level){
 	high_h = config.high_h;
 	high_s = config.high_s;
 	high_v = config.high_v;
+	opening_closing_mat_point = config.opening_closing_mat_point;
+	opening_iter = config.opening_iter;
+	closing_iter = config.closing_iter;
 }
 
 void imageCallback(const sensor_msgs::Image::ConstPtr& msg){
 	cv_bridge::CvImagePtr cv_img_ptr;
-	try{	
+	try{
 		cv_img_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
 		cv::Mat image = cv_img_ptr->image;
 		cv::Mat image_marked = cv_img_ptr->image;
-		
+
 		if(!image.empty()){
-		
 			cv::Mat image_hsv;
 			cv::Mat image_thresholded;
 			cv::cvtColor(image, image_hsv, cv::COLOR_BGR2HSV);
-		
 			ROS_INFO("Thresholding Values: (%d %d %d) - (%d %d %d): ", low_h, low_s, low_v, high_h, high_s, high_v);
-		
 			if(!(high_h<=low_h || high_s<=low_s || high_v<=low_v)) {
-		
 				image_thresholded = vision_commons::Threshold::threshold(image, low_h, low_s, low_v, high_h, high_s, high_v);
-
-				//image_thresholded = vision_commons::Morph::open(image_thresholded, 3, 1, 1, 2);
-				//image_thresholded = vision_commons::Morph::close(image_thresholded, 3, 1, 1, 2);
-				
+				image_thresholded = vision_commons::Morph::open(image_thresholded, 2*opening_closing_mat_point+1, opening_closing_mat_point, opening_closing_mat_point, opening_iter);
+				image_thresholded = vision_commons::Morph::close(image_thresholded, 2*opening_closing_mat_point+1, opening_closing_mat_point, opening_closing_mat_point, closing_iter);
 				cv_bridge::CvImage thresholded_ptr;
 				thresholded_ptr.header = msg->header;
 				thresholded_ptr.encoding = sensor_msgs::image_encodings::MONO8;
 				thresholded_ptr.image = image_thresholded;
 				thresholded_HSV_pub.publish(thresholded_ptr.toImageMsg());
-				
 				std::vector<std::vector<cv::Point> > contours = vision_commons::Contour::getBestX(image_thresholded, 2);
-
 				ROS_INFO("contours size = %d", contours.size());
 				if (contours.size() != 0){
-					int index = 0; 
-					int index2 = 1;
 					// calculating center of mass of contour using moments
 					std::vector<cv::Moments> mu(1);
-					mu[0] = moments(contours[index], false);
+					mu[0] = moments(contours[0], false);
 					std::vector<cv::Point2f> mc(1);
 					mc[0] = cv::Point2f( mu[0].m10/mu[0].m00 , mu[0].m01/mu[0].m00 );
-					cv::Rect bounding_rectangle = cv::boundingRect(cv::Mat(contours[index]));
-					if(contours.size() > 1 && index2 != -1) {
-						cv::Rect bounding_rectangle2 = cv::boundingRect(cv::Mat(contours[index2]));
+					int index = 0;
+					cv::Rect bounding_rectangle = cv::boundingRect(cv::Mat(contours[0]));
+					if(contours.size() >= 2) {
+						cv::Rect bounding_rectangle2 = cv::boundingRect(cv::Mat(contours[1]));
 						if((bounding_rectangle2.br().y + bounding_rectangle2.tl().y) > (bounding_rectangle.br().y + bounding_rectangle.tl().y)) {
-							index = index2;
+							index = 1;
 							bounding_rectangle = bounding_rectangle2;
 						}
 					}
 					std::vector<cv::Point2f>center(1);
 					std::vector<float>radius(1);
-					cv::minEnclosingCircle(contours[index],center[0],radius[0]); 
+					cv::minEnclosingCircle(contours[index], center[0], radius[0]);
 					cv::circle(image_marked, center[0], (int)radius[0], cv::Scalar(180,180,180), 2, 8, 0);
 					// publish coordinates message
 					geometry_msgs::PointStamped buoy_point_message;
@@ -94,7 +88,6 @@ void imageCallback(const sensor_msgs::Image::ConstPtr& msg){
 					buoy_point_message.point.z = ((float)image.size().height)/2 - (bounding_rectangle.br().y + bounding_rectangle.tl().y)/2;
 					ROS_INFO("Buoy Location (x, y, z) = (%.2f, %.2f, %.2f)", buoy_point_message.point.x, buoy_point_message.point.y, buoy_point_message.point.z);
 					coordinates_pub.publish(buoy_point_message);
-
 					cv::RotatedRect minEllipse;
 					cv::cvtColor(image_marked, image_marked, cv::COLOR_BGR2HSV);
 					if(contours[index].size()>=5){
@@ -104,12 +97,11 @@ void imageCallback(const sensor_msgs::Image::ConstPtr& msg){
 					cv::circle(image_marked, cv::Point((bounding_rectangle.br().x + bounding_rectangle.tl().x)/2, (bounding_rectangle.br().y + bounding_rectangle.tl().y)/2), 1, cv::Scalar(255,100,100), 8, 0);
 					cv::circle(image_marked, cv::Point(image.size().width/2, image.size().height/2), 1, cv::Scalar(255,100, 50), 8, 0);
 
-					for( int i = 0; i< contours.size(); i++ )
-				    {
-        				cv::Scalar color = cv::Scalar(255,255,100);
-        				cv::drawContours( image_marked, contours, i, color, 1, 8, std::vector<cv::Vec4i>(), 0, cv::Point() );
-        			   // cv::drawContours( image_marked, hull, i, color, 1, 8, std::vector<cv::Vec4i>(), 0, cv::Point() );
-      				}
+					for( int i = 0; i< contours.size(); i++ ) {
+						cv::Scalar color = cv::Scalar(255,255,100);
+						cv::drawContours( image_marked, contours, i, color, 1, 8, std::vector<cv::Vec4i>(), 0, cv::Point() );
+						// cv::drawContours( image_marked, hull, i, color, 1, 8, std::vector<cv::Vec4i>(), 0, cv::Point() );
+					}
 
 					cv_bridge::CvImage marked_ptr;
 					marked_ptr.header = msg->header;
@@ -119,7 +111,7 @@ void imageCallback(const sensor_msgs::Image::ConstPtr& msg){
 				}
 			}
 		}
-	}	
+	}
 	catch(cv_bridge::Exception &e){
 		ROS_ERROR("cv_bridge exception: %s", e.what());
 	}
@@ -138,7 +130,7 @@ int main(int argc, char **argv){
 	image_transport::ImageTransport it(nh);
 	thresholded_HSV_pub = it.advertise("/thresholded", 1);
 	marked_pub = it.advertise("/marked",1);
-	coordinates_pub = nh.advertise<geometry_msgs::PointStamped>("/threshold/center_coordinates", 1000);
+	coordinates_pub = nh.advertise<geometry_msgs::PointStamped>("/buoy_task/buoy_coordinates", 1000);
 	image_transport::Subscriber image_raw_sub = it.subscribe("/hardware_camera/cam_lifecam/image_raw", 1, imageCallback);
 	ros::spin();
 	return 0;
