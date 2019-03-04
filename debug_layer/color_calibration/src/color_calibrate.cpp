283d2807fbbbc616b3ba7bea732b20876719a461
ksakash <akashsingh049811@gmail.com>
2019-03-05 01:37:21 +0530

    color calibration tested

--- debug_layer/color_calibration/src/color_calibrate.cpp
+++ debug_layer/color_calibration/src/color_calibrate.cpp
@@ -2,10 +2,13 @@
 #include <iostream>
 #include <dynamic_reconfigure/server.h>
 #include <color_calibration/visionConfig.h>
+#include <color_calibration/Dump.h>
 
 int r_min, r_max, g_min, g_max, b_min, b_max;
 int opening_mat_point, opening_iter, closing_mat_point, closing_iter;
 int bilateral_iter;
+
+bool save_params = false;
 
 void callback (color_calibration::visionConfig &config, uint32_t level) {
     r_min = config.r_min;
@@ -22,18 +25,42 @@ void callback (color_calibration::visionConfig &config, uint32_t level) {
 
     bilateral_iter = config.bilateral_iter;
 
+    if (config.save_params) {
+        save_params = true;
+        config.save_params = false;
+    }
+
     ROS_INFO("A color calibration configuration request");
 }
 
 int main (int argc, char** argv) {
     ros::init (argc, argv, "color_calibrate");
+    ros::NodeHandle nh;
 
     dynamic_reconfigure::Server<color_calibration::visionConfig> server;
     dynamic_reconfigure::Server<color_calibration::visionConfig>::CallbackType f;
     f = boost::bind(&callback, _1, _2);
     server.setCallback(f);
 
-    ros::spin();
+    ros::ServiceClient client = nh.serviceClient<color_calibration::Dump>("dump_parameters");
+    color_calibration::Dump dump_srv;
+
+    ros::Rate loop_rate(50);
+
+    while (ros::ok()) {
+
+        if (save_params) {
+            nh.getParam("color_calibrate_object", dump_srv.request.filename);
+            if (client.call(dump_srv))
+            {
+                ROS_INFO("Parameters dumped");
+            }
+            save_params = false;
+        }
+        
+        loop_rate.sleep();
+        ros::spinOnce();
+    }
 
     return 0;
 }
