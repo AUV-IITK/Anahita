fe7ad99e3f5a5da6c8e96470b97c8edea4e08c17
ksakash <akashsingh049811@gmail.com>
2019-04-07 04:07:08 +0530

    minor fixes in odom_dvl_imu

--- master_layer/setup.py
+++ master_layer/setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 setup_args = generate_distutils_setup(
     packages=[],
     package_dir={},
-    requires=['rospy']
+    requires=['rospy', 'geometry_msgs']
 )
 
 setup(**setup_args)
