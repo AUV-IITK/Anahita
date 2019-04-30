3546ba69776d2e9d2cd7b6008ba9ec2978d8ca20
ksakash <akashsingh049811@gmail.com>
2019-04-30 19:05:20 +0530

    fixed serial node

--- hardware_layer/temporary_arduino/src/master.py
+++ hardware_layer/temporary_arduino/src/master.py
@@ -8,48 +8,14 @@ from std_msgs.msg import Float32
 from anahita_msgs.msg import Thrust
 from roslib.message import get_message_class
 
-current_values = []
+pwm_str = ""
 
-def ros_next(rate_hz):
-    prev_time = rospy.get_time()
-    timeout = 1 / rate_hz
-    curr_time = rospy.get_time()
-    if curr_time - prev_time > timeout:
-        prev_time = curr_time
-        return True
-    else:
-        return False
-
-def callback(data):
-    current_values = [data.sideward_front, data.sideward_back, data.forward_right, data.forward_left, data.upward_north_east, data.upward_north_west, data.upward_south_east, data.upward_south_west, data.marker_dropper, data.torpedo]
-    print(current_values[2])
-    rospy.loginfo("Listening to a new message")
-
-
-# Read the serial message string, and publish to the correct topics
-def process_message(line):
-    try:
-        values = line.decode().split(',')
-        pairs = {k: v for k, v in zip(sensor_csv_headers, values)}
-       
-        # Zip values with the corresponding environmental variable
-        variable_values = values[1:]
-        pairs = zip(sensor_csv_headers, variable_values)
-
-        return pairs
-
-    except IndexError:
-        rospy.logwarn("Short read, received part of a message: {}".format(inputBuffer.decode()))
-        serial_connection.close()
-        serial_connection.open()
-        return
-
-    # Occasionally, we get rotten bytes which couldn't decode
-    except UnicodeDecodeError:
-        rospy.logwarn("Received weird bits, ignoring: {}".format(inputBuffer))
-        serial_connection.close()
-        serial_connection.open()
-        return
+def callback(msg):
+    global pwm_str
+    pwm_str = str(msg.sideward_front) + ',' + str(msg.sideward_back) + ',' + \
+                str(msg.forward_left) + ',' + str(msg.forward_right) + ',' + \
+                str(msg.upward_north_east) + ',' + str(msg.upward_north_west) + ',' + \
+                str(msg.upward_south_east) + ',' + str(msg.upward_south_west) + '\n'
 
 if __name__ == '__main__':
     
@@ -57,70 +23,36 @@ if __name__ == '__main__':
     
     rospy.Subscriber("/pwm", Thrust, callback)
     depth_publisher = rospy.Publisher("/depth", Float32, queue_size=10)
-    sensor_csv_headers = ["status","pressure"]
 
-    actuator_csv_headers = ["status", "thruster_e", "thruster_w", "thruster_n",
-                            "thruster_s", "thruster_ne", "thruster_nw", "thruster_se",
-                            "thruster_sw","marker_dropper","torpedo"]  
+    # serial_port_id = rospy.get_param("~serial_port_id", "/dev/arduino")
+    # baud_rate = rospy.get_param("~baud_rate", 115200)
 
-    VALID_SENSOR_VARIABLES = ["pressure_sensor"]
-
-    serial_port_id = rospy.get_param("~serial_port_id", "/dev/arduino")
-    publisher_rate_hz = rospy.get_param("~publisher_rate_hz", 1)
-    baud_rate = rospy.get_param("~baud_rate", 115200)
-
-    timeout_s = 1 / publisher_rate_hz
+    sensor_msg = Float32()
 
     # Initialize the serial connection
     try:
-        serial_connection = serial.Serial(serial_port_id, baud_rate, timeout=timeout_s)
+        serial_connection = serial.Serial("/dev/ttyACM0", 115200, timeout=5)
+        rospy.sleep(2)
         
     except:
         rospy.logerr("Could not start serial_connection")
         exit()
 
-    publish_time = ros_next(publisher_rate_hz)
-
     while not rospy.is_shutdown():
 
-        # Read before writing
-        rospy.loginfo("Inside the spin loop")
-        try:
-            print("Going to listen")
-            inputBuffer = serial_connection.readline()
-            print("input coming: " + str(inputBuffer) + "lol")
-        except: 
-            rospy.logerr("Failed, could not read the serial connection")
+        rospy.loginfo('Inside the spin loop')
 
-        # Generate the message for the current state
-        # status, thrusters one after another
-        try:
-            message = "0,{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}\n".format(
-                current_values[0],
-                current_values[1],
-                current_values[2],
-                current_values[3],
-                current_values[4],
-                current_values[5],
-                current_values[6],
-                current_values[7],
-                current_values[8],
-                current_values[9],    
-            ).encode('utf-8')
-            print(message)
-            serial_connection.write(message)
-            serial_connection.flush()
+        serial_connection.flush()
+        pwm_msg = pwm_str.encode('utf-8')
+        serial_connection.write(pwm_msg)
+        rospy.sleep(0.05)
 
-        except:
-            rospy.logerr("Start the pwm publisher message")
+        # Serial read section
+        msg = serial_connection.read(serial_connection.inWaiting()) # read all characters in buffer
+        sensor_msg.data = float(msg)
+        depth_publisher.publish(sensor_msg)
+        print msg
 
-        
-        pairs = process_message(inputBuffer)
-        
-        if publish_time and pairs is not None:
-        
-            for header, value in pairs:
-                depth_publisher.publish(value)
 
     rospy.spin()
 
