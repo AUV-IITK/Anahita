e8d6d194613634246c2ad0f8f33ec8d61c1e2b75
ksakash <akashsingh049811@gmail.com>
2019-05-14 06:52:52 +0530

    removed redundancies

--- vision_layer/vision_tasks/include/gate.h
+++ vision_layer/vision_tasks/include/gate.h
@@ -6,13 +6,11 @@
 class Gate : public Base_class
 {
 public:
-    using Base_class::frontTaskHandling;
     Gate();
     ~Gate();
     virtual void loadParams () override;
     virtual void spinThreadFront () override;
     virtual void spinThreadBottom () override;
-    // void frontTaskHandling(bool status);
 };
 
 #endif // GATE_TASK_H
--- vision_layer/vision_tasks/include/marker.h
+++ vision_layer/vision_tasks/include/marker.h
@@ -6,13 +6,11 @@
 class Marker : public Base_class
 {
 public:
-    using Base_class::frontTaskHandling;
     Marker();
     ~Marker();
     virtual void loadParams () override;
     virtual void spinThreadFront () override;
     virtual void spinThreadBottom () override;
-    // void frontTaskHandling(bool status);
 
 private:
     image_transport::Publisher front_roi_pub;
