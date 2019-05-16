650c4b1c093ffe0093a45a4d60f970c374a9e5df
Ayush Gupta <7ayushgupta@gmail.com>
2019-05-16 07:30:11 +0000

    Added package for xsens_MT300i driver

---
+++ navigation_layer/mapping/include/mapping_main.h
@@ -0,0 +1,9 @@
+#include <ros/ros.h>
+#include <grid_map_ros/grid_map_ros.hpp>
+#include <grid_map_msgs/GridMap.h>
+#include <cmath>
+#include "yaml-cpp/yaml.h"
+#include <tf/transform_broadcaster.h>
+#include <tf/transform_datatypes.h>
+#include<string>
+#include "nav_msgs/Odometry.h"
--- navigation_layer/mapping/src/mapping_main.cpp
+++ navigation_layer/mapping/src/mapping_main.cpp
@@ -1,12 +1,4 @@
-#include <ros/ros.h>
-#include <grid_map_ros/grid_map_ros.hpp>
-#include <grid_map_msgs/GridMap.h>
-#include <cmath>
-#include "yaml-cpp/yaml.h"
-#include <tf/transform_broadcaster.h>
-#include <tf/transform_datatypes.h>
-#include<string>
-#include "nav_msgs/Odometry.h"
+#include "mapping_main.h"
 
 using namespace grid_map;
 GridMap map({"elevation", "occupancy"});
