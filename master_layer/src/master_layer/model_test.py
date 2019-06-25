5f0af0cdd559550f43882a03d7e8bfc6fa4d6beb
ksakash <akashsingh049811@gmail.com>
2019-06-25 09:59:17 +0530

    added separate script for model test

--- master_layer/src/master_layer/model_test.py
+++ master_layer/src/master_layer/model_test.py
@@ -15,7 +15,7 @@ import h5py
 num_attr = 15 # number of features fed into the network
 
 # load dataset
-dataframe = pandas.read_csv("depth_data.csv", delim_whitespace=True, header=None)
+dataframe = pandas.read_csv("depth_test.csv", delim_whitespace=True, header=None)
 dataset = dataframe.values
 print ('data loaded')
 
@@ -35,23 +35,11 @@ def baseline_model():
     model.compile(loss='mean_squared_error', optimizer='adam')
     return model
 
-# fit the model
-regressor = KerasRegressor(build_fn=baseline_model, batch_size=20, epochs=1)
-print ('model fit start')
-regressor.fit(X, Y)
-print ('model fit end')
+# Instantiate the model as you please (we are not going to use this)
+model2 = KerasRegressor(build_fn=baseline_model, batch_size=5, epochs=100, verbose=1)
 
-# evaluate model with standardized dataset
-seed = 7
-numpy.random.seed(seed)
-kfold = KFold(n_splits=10, random_state=seed)
-results = cross_val_score(regressor, X, Y, cv=kfold)
-print("Results: %.2f (%.2f) MSE" % (results.mean(), results.std()))
+# This is where you load the actual saved model into new variable
+model2.model = load_model("/home/ironman/anahita_ws/src/Anahita/master_layer/src/master_layer/depth_model.h5")
 
-# serialize model to JSON
-depth_model_json = regressor.model.to_json()
-with open("model.json", "w+") as json_file:
-    json_file.write(depth_model_json)
-# serialize weights to HDF5
-regressor.model.save_weights("depth_model.h5")
-print("Saved model to disk")
+# Now you can use this to predict on new data (without fitting model2, because it uses the older saved model)
+model2.predict(X)
